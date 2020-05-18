82f3ae591ff15f21b3ea51bf05d67002fac74028
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-05-19 00:38:50 +0700

    storing image to remaining data

--- legacyapp/src/core/actions/places/index.js
+++ legacyapp/src/core/actions/places/index.js
@@ -2,18 +2,31 @@ import { ADD_PLACE, DELETE_PLACE } from '../../type/places';
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location: location
-    };
-    fetch('https://awesome-place-app-277412.firebaseio.com/places.json', {
+    fetch('https://us-central1-awesome-place-app-277412.cloudfunctions.net/storeImage', {
       method: 'POST',
-      body: JSON.stringify(placeData)
+      body: JSON.stringify({
+        image: image.base64
+      })
     })
     .catch(err => console.log(err))
     .then(res => res.json())
-    .then(parsedRed => {
-      console.log(parsedRed);
+    .then(parsedRes => {
+      const placeData = {
+        name: placeName,
+        location: location,
+        image: parsedRes.imageUrl
+      };
+      return (
+        fetch('https://awesome-place-app-277412.firebaseio.com/places.json', {
+          method: 'POST',
+          body: JSON.stringify(placeData)
+        })
+      );
+    })
+    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(parsedRes => {
+      console.log(parsedRes);
     });
   };
 };
